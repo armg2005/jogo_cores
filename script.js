a0b5934864e1cc73b046f1bad8e9cbb8a09763e5
Alisson Ricady <armg@aluno.ifnmg.edu.br>
2025-07-31 14:56:44 -0300

    adincionando algumas funções

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+function sortearNovaCorAlvo(coresSelecionadas) {
+  const indiceSorteado = Math.floor(Math.random() * coresSelecionadas.length);
+  corAlvo = coresSelecionadas[indiceSorteado];
+  document.getElementById("cor_escolha").style.backgroundColor = corAlvo;
+}
 function pegar9Embaralhados(array) {
   let embaralhado = embaralhar(array);
   return embaralhado.slice(0, 9); 
@@ -9,33 +14,37 @@ function embaralhar(array) {
     let j = Math.floor(Math.random() * (i + 1));
     [copia[i], copia[j]] = [copia[j], copia[i]]; // troca os elementos
   }
-
+  
   return copia;
 }
 
+
 document.addEventListener("DOMContentLoaded", function() {
-  let cores = ["blue", "red", "green", "black", "orange", "yellow", "orange", "purple", "pink", "brown", "gray", "cyan"];
-  
-  function sortearNovaCorAlvo() {
-    const indiceSorteado = Math.floor(Math.random() * coresSelecionadas.length);
-    corAlvo = coresSelecionadas[indiceSorteado];
-    document.getElementById("cor_escolha").style.backgroundColor = corAlvo;
-  }
-  let coresSelecionadas = pegar9Embaralhados(cores);
-  let pontuacao = 0;
 
   
-  for (let i = 0; i < 9; i++) {
+  let cores = ["blue", "red", "green", "black", "orange", "yellow", "orange", "purple", "pink", "brown", "gray", "cyan"];
+  const botao_jogar = document.getElementById("botao_jogar");
+  let pontuacao = 0;
+  
+  let coresSelecionadas = pegar9Embaralhados(cores);
+  
+  botao_jogar.onclick =function(){
+    let coresSelecionadas = pegar9Embaralhados(cores);  
+    sortearNovaCorAlvo(coresSelecionadas);
+    for (let i = 0; i < 9; i++) {
     let div = document.getElementById("div" + (i + 1));
-    
+
     div.style.backgroundColor = coresSelecionadas[i];
     div.onclick = function() {
       const corClicada = div.style.backgroundColor;
-      if(corClicada == corAlvo){
-        alert("parabeins")
-      }
-    };
-  }
-   sortearNovaCorAlvo();
-
+      if (corClicada === corAlvo) {
+          alert("Parabéns, você acertou!");
+        } else {
+          alert("Errou, tente novamente.");
+        }
+      };
+    }
+ 
+  };
 });
+  
