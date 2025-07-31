ad183596af1709e9ed2ae7fb53b46922cf23699b
Alisson Ricady <armg@aluno.ifnmg.edu.br>
2025-07-31 17:54:28 -0300

    parte inicialfinalizada . falta pontuação e parte bônus

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 function sortearNovaCorAlvo(coresSelecionadas) {
   const indiceSorteado = Math.floor(Math.random() * coresSelecionadas.length);
-  corAlvo = coresSelecionadas[indiceSorteado];
+  const corAlvo = coresSelecionadas[indiceSorteado];
   document.getElementById("cor_escolha").style.backgroundColor = corAlvo;
+  return corAlvo; 
 }
+
 function pegar9Embaralhados(array) {
   let embaralhado = embaralhar(array);
   return embaralhado.slice(0, 9); 
@@ -18,32 +20,53 @@ function embaralhar(array) {
   return copia;
 }
 
-
-document.addEventListener("DOMContentLoaded", function() {
-
-  
-  let cores = ["blue", "red", "green", "black", "orange", "yellow", "orange", "purple", "pink", "brown", "gray", "cyan"];
-  const botao_jogar = document.getElementById("botao_jogar");
-  let pontuacao = 0;
-  
-  let coresSelecionadas = pegar9Embaralhados(cores);
-  
-  botao_jogar.onclick =function(){
-    let coresSelecionadas = pegar9Embaralhados(cores);  
-    sortearNovaCorAlvo(coresSelecionadas);
-    for (let i = 0; i < 9; i++) {
+function atualizar(coresSelecionadas, corAlvo, cores) {
+  for (let i = 0; i < 9; i++) {
     let div = document.getElementById("div" + (i + 1));
-
     div.style.backgroundColor = coresSelecionadas[i];
+
+    const novasCores = pegar9Embaralhados(cores);
+    const novaCorAlvo = sortearNovaCorAlvo(novasCores);
     div.onclick = function() {
       const corClicada = div.style.backgroundColor;
       if (corClicada === corAlvo) {
-          alert("Parabéns, você acertou!");
-        } else {
-          alert("Errou, tente novamente.");
-        }
-      };
-    }
+        alert("Parabéns, você acertou!");
+        atualizar(novasCores, novaCorAlvo,cores);
+      } else {
+        alert("Errou, tente novamente.");
+        atualizar(novasCores, novaCorAlvo, cores);
+      }
+
+    };
+  }
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+
+  let cores = ["blue", "red", "green", "black", "orange", "yellow", "orange", "purple", "pink", "brown", "gray", "cyan"];
+  const botao_jogar = document.getElementById("botao_jogar");
+  let pontuacao = 0;
+  const temporizador = document.getElementById("temporizador");
+  let intervalo;
+
+  botao_jogar.onclick =function(){
+
+    let tempo = 40;
+    temporizador.textContent = tempo;
+    clearInterval(intervalo);
+
+    intervalo = setInterval(function () {
+      tempo--;
+      temporizador.textContent = "Tempo Restante:"+ tempo;
+
+      if (tempo <= 0) {
+        clearInterval(intervalo);
+        alert(" Tempo esgotado!");
+      }
+    }, 1000);
+    let coresSelecionadas = pegar9Embaralhados(cores);  
+    let corAlvo =sortearNovaCorAlvo(coresSelecionadas);
+    atualizar(coresSelecionadas ,corAlvo, cores)
  
   };
 });
